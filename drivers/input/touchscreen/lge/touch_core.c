f775b0ab490559304b9d9d46dd1e792d77af5413
Diab Neiroukh <lazerl0rd@thezest.dev>
2020-08-11 02:22:14 +0100

    input: touchscreen: lge: Require low latency
    Suggested-by: Siarhei Vishniakou <svv@google.com>
    Signed-off-by: Diab Neiroukh <lazerl0rd@thezest.dev>
    Signed-off-by: SGCMarkus <markusornik@gmail.com>

--- drivers/input/touchscreen/lge/touch_core.c
+++ drivers/input/touchscreen/lge/touch_core.c
@@ -192,9 +192,12 @@ irqreturn_t touch_irq_thread(int irq, void *dev_id)
 
 	TOUCH_TRACE();
 
+	pm_qos_update_request(&ts->pm_qos_req, 100);
 #if defined(CONFIG_SECURE_TOUCH)
-	if (secure_touch_filter_interrupt(ts) == IRQ_HANDLED)
+	if (secure_touch_filter_interrupt(ts) == IRQ_HANDLED) {
+		pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 		return IRQ_HANDLED;
+	}
 #endif
 	mutex_lock(&ts->lock);
 
@@ -250,6 +253,7 @@ irqreturn_t touch_irq_thread(int irq, void *dev_id)
 	}
 
 	mutex_unlock(&ts->lock);
+	pm_qos_update_request(&ts->pm_qos_req, PM_QOS_DEFAULT_VALUE);
 
 	return IRQ_HANDLED;
 }
@@ -1043,6 +1047,11 @@ static int touch_core_probe_normal(struct platform_device *pdev)
 		goto error_init_input;
 	}
 
+	ts->pm_qos_req.type = PM_QOS_REQ_AFFINE_IRQ;
+	ts->pm_qos_req.irq = ts->irq;
+	pm_qos_add_request(&ts->pm_qos_req, PM_QOS_CPU_DMA_LATENCY,
+			   PM_QOS_DEFAULT_VALUE);
+
 	ret = touch_request_irq(ts->irq, touch_irq_handler,
 			touch_irq_thread, ts->irqflags | IRQF_ONESHOT,
 			LGE_TOUCH_NAME, ts);
@@ -1069,6 +1078,7 @@ static int touch_core_probe_normal(struct platform_device *pdev)
 
 error_request_irq:
 	free_irq(ts->irq, ts);
+	pm_qos_remove_request(&ts->pm_qos_req);
 error_init_input:
 	if (ts->input) {
 		input_mt_destroy_slots(ts->input);
